78788e76f3275870d842c710e51cbe902b0f7c4a
DeepikaKrishnaa <saideepikarangavajhula@gmail.com>
2026-01-18 20:40:04 +0530

    Paths Served

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const taskRoutes = require("./routes/tasks");
 
 app.use(express.json());
 app.use(cors());
+
+const taskRoutes = require("./routes/tasks");
 app.use("/api", taskRoutes);
 
 const authRoutes = require("./routes/auth");
-
 app.use("/api", authRoutes);
+
+app.use(express.static("frontend"));
+app.use(express.static("assets"));
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
