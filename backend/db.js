8467bf410db3dc4aec974924033e8de087b52902
DeepikaKrishnaa <saideepikarangavajhula@gmail.com>
2026-01-18 19:22:07 +0530

    Updated DB

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
     console.error("DB connection failed:", err);
-    return;
+  } else {
+    console.log("MySQL DB connected successfully");
   }
-  console.log("Connected to MySQL database");
 });
 
 module.exports = db;
