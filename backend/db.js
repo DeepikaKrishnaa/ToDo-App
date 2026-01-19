0500075844fae792c502a68e6c3052e6a36120ab
DeepikaKrishnaa <saideepikarangavajhula@gmail.com>
2026-01-19 10:27:30 +0530

    Updated DB

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 db.connect((err) => {
   if (err) {
